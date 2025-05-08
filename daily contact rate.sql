eabca456ae85bccf1651bfd76380133c47b92044
karen-dellow-careabout <karen.dellow@careabout.com.au>
2025-05-08 17:09:52 +1000

    Create daily contact rate.sql

---
+++ daily contact rate.sql
@@ -0,0 +1,156 @@
+WITH LeadRequestMod AS (
+  SELECT 
+    Object_lr,
+    LeadReqId_lr,
+    CreatedDateTimeLeadReq_lr,
+    CAST(CreatedDateTimeLeadReq_lr AT TIME ZONE 'UTC' AT TIME ZONE 'AUS Eastern Standard Time' AS datetime) AS CreatedDateTime_AEST,
+    CAST(CreatedDateTimeLeadReq_lr AS date) AS CreatedDateOnly,
+    FullName_lr,
+    FirstName_lr,
+    LastName_lr,
+    Email_lr,
+    CompanyName_lr,
+    LeadType_lr,
+    LeadSource_lr,
+    CustomerPostalCode_lr,
+    CustomerCitySuburb_lr,
+    ProductInterest_lr,
+    CampaignName_lr,
+    HcpLevel_lr,
+    HcAssessmentStage_lr,
+    Status_lr,
+    WhoNeedsHelp_lr, 
+    GaConnectorLastClickCampaign_lr,
+    GaConnectorLastClickSource_lr,
+    GaConnectorLastClickLandingPage_lr,
+    GaConnectorLastClickChannel_lr,
+    GaConnectorFirstClickChannel_lr,
+    GaConnectorFirstClickMedium_lr,
+    GaConnectorFirstClickSource_lr,
+    MarketingCampaign_lr,
+    MarketingChannel_lr,
+    ClickSource_lr,
+    AllTrafficSources_lr,
+    Coverage_lr,
+    CASE
+        WHEN CONVERT(VARCHAR(8), CreatedDateTimeLeadReq_lr, 108) BETWEEN '09:00:00' AND '19:00:00'
+            THEN 1
+        ELSE 0
+    END AS WithinTimeWindow,
+    ROW_NUMBER() OVER (PARTITION BY CompanyName_lr  ORDER BY CreatedDateTimeLeadReq_lr desc) AS seq
+  FROM [careabout-db].dbo.ViewLeadRequestMod
+  WHERE   CreatedDateTimeLeadReq_lr BETWEEN 
+  DATEADD(HOUR, -10, '2025-05-07 00:00:00.000') 
+  AND 
+  DATEADD(HOUR, -10, '2025-05-07 23:59:59.000')
+    AND HcAssessmentStage_lr IN ('Newly Funded','Switching','Newly Funded - Scheduled')
+    AND Coverage_lr = 'Coverage'
+    AND Status_lr NOT IN ('Duplicate', 'Invalid details')
+    AND (CampaignName_lr = 'Homecare All' OR CampaignName_lr IS NULL)   
+)
+
+
+,LeadRequestMod_dedupe as (
+select
+    Object_lr,
+    LeadReqId_lr,
+    CreatedDateTimeLeadReq_lr,
+    CreatedDateOnly,
+    FullName_lr,
+    FirstName_lr,
+    LastName_lr,
+    Email_lr,
+    CompanyName_lr,
+    LeadType_lr,
+    LeadSource_lr,
+    CustomerPostalCode_lr,
+    CustomerCitySuburb_lr,
+    ProductInterest_lr,
+    CampaignName_lr,
+    HcpLevel_lr,
+    HcAssessmentStage_lr,
+    Status_lr,
+    WhoNeedsHelp_lr, 
+    GaConnectorLastClickCampaign_lr,
+    GaConnectorLastClickSource_lr,
+    GaConnectorLastClickLandingPage_lr,
+    GaConnectorLastClickChannel_lr,
+    GaConnectorFirstClickChannel_lr,
+    GaConnectorFirstClickMedium_lr,
+    GaConnectorFirstClickSource_lr,
+    MarketingCampaign_lr,
+    MarketingChannel_lr,
+    ClickSource_lr,
+    AllTrafficSources_lr,
+    Coverage_lr,
+   WithinTimeWindow
+FROM LeadRequestMod
+WHERE seq = 1
+)
+
+
+,FirstContactLeadsReq AS (
+  SELECT LeadId as LeadRequestId,
+    CAST(CreatedDate AT TIME ZONE 'UTC' AT TIME ZONE 'AUS Eastern Standard Time' AS datetime) AS CreatedDateTime_AEST,
+    CAST(CreatedDate AT TIME ZONE 'UTC' AT TIME ZONE 'AUS Eastern Standard Time' AS date) AS CreatedDate_AEST
+  FROM [careabout-db].dbo.LeadHistory
+  WHERE Field = 'Status' AND NewValue = 'Contacted x1' 
+  
+UNION ALL
+
+  SELECT ParentId as LeadRequestId,
+    CAST(CreatedDate AT TIME ZONE 'UTC' AT TIME ZONE 'AUS Eastern Standard Time' AS datetime) AS CreatedDateTime_AEST,
+    CAST(CreatedDate AT TIME ZONE 'UTC' AT TIME ZONE 'AUS Eastern Standard Time' AS date) AS CreatedDate_AEST
+  FROM [careabout-db].dbo.RequestHistory
+  WHERE Field = 'Request_Status__c' AND NewValue IN ('Closed - Actioned') 
+)
+
+
+SELECT 
+  lr.*,
+  lh.CreatedDate_AEST AS FirstContactDate,
+  lh.CreatedDateTime_AEST,
+  CASE
+        WHEN CONVERT(VARCHAR(8), lh.CreatedDateTime_AEST, 108) BETWEEN '09:00:00' AND '19:00:00'
+            THEN 1
+        ELSE 0
+    END AS ContactedWithinDay
+FROM LeadRequestMod_dedupe lr
+LEFT JOIN FirstContactLeadsReq lh
+  ON lr.LeadReqId_lr = lh.LeadRequestId AND
+  lr.CreatedDateOnly = lh.CreatedDate_AEST
+  GROUP BY 
+  lr.Object_lr,
+  lr.LeadReqId_lr,
+  CreatedDateTimeLeadReq_lr,
+  lr.CreatedDateOnly,
+  lr.FullName_lr,
+  lr.FirstName_lr,
+  lr.LastName_lr,
+  lr.Email_lr,
+  lr.CompanyName_lr,
+  lr.LeadType_lr,
+  lr.LeadSource_lr,
+  lr.CustomerPostalCode_lr,
+  lr.CustomerCitySuburb_lr,
+  lr.ProductInterest_lr,
+  lr.CampaignName_lr,
+  lr.HcpLevel_lr,
+  lr.HcAssessmentStage_lr,
+  lr.Status_lr,
+  lr.WhoNeedsHelp_lr, 
+  lr.GaConnectorLastClickCampaign_lr,
+  lr.GaConnectorLastClickSource_lr,
+  lr.GaConnectorLastClickLandingPage_lr,
+  lr.GaConnectorLastClickChannel_lr,
+  lr.GaConnectorFirstClickChannel_lr,
+  lr.GaConnectorFirstClickMedium_lr,
+  lr.GaConnectorFirstClickSource_lr,
+  lr.MarketingCampaign_lr,
+  lr.MarketingChannel_lr,
+  lr.ClickSource_lr,
+  lr.AllTrafficSources_lr,
+  lr.Coverage_lr,
+  lh.CreatedDate_AEST,
+  lh.CreatedDateTime_AEST,
+  lr.WithinTimeWindow
